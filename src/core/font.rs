039317036710d78b6d1721aeb647bfa9cebb8416
solvixx <solvixx@proton.me>
2026-05-30 13:03:36 +0300

    fix: conditionally import Command for Linux

--- src/core/font.rs
+++ src/core/font.rs
@@ -1,6 +1,8 @@
 use directories::ProjectDirs;
 use std::fs;
 use std::path::{Path, PathBuf};
+#[cfg(target_os = "linux")]
+use std::process::Command;
 use typst::foundations::Bytes;
 use typst::text::{Font as TypstFont, FontBook};
 use typst::utils::LazyHash;
